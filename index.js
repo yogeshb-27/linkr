c99a93e4619c9359c86c72e81671bb01f054c40c
yogesh <bidaveyogesh@gmail.com>
2024-02-12 19:48:06 +0530

    create express.js app

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/", (req, res) => res.end("HOME"));
+
+app.listen(port, () => {
+  console.log(`server up at port: ${port} !`);
+});
