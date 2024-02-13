0fff0a236162ab609df9b58988eb3584d77c4b35
yogesh <bidaveyogesh@gmail.com>
2024-02-13 14:50:59 +0530

    add client online offline toast

--- public/script.js
+++ public/script.js
@@ -8,3 +8,17 @@ const showToast = (icon, message, color) => {
     toast.innerHTML = "";
   }, 3000);
 };
+window.addEventListener("online", () =>
+  showToast(
+    "<i class='bx bx-wifi text-success me-2'></i>",
+    "You Are Online",
+    "text-success "
+  )
+);
+window.addEventListener("offline", () =>
+  showToast(
+    "<i class='bx bx-wifi-off text-danger me-2'></i>",
+    "You Are Offline",
+    "text-danger"
+  )
+);
